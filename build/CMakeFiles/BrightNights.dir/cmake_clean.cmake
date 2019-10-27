2f6bd778d664de770635591919ab376ca7b0a6ba
ParanormalVibe <foo@fennel.io>
2019-10-26 21:19:47 -0400

    initial commit

---
+++ build/CMakeFiles/BrightNights.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../bin/BrightNights.dll"
+  "../bin/BrightNights.dll.manifest"
+  "../bin/BrightNights.pdb"
+  "CMakeFiles/BrightNights.dir/src/cwmods.cpp.obj"
+  "CMakeFiles/BrightNights.dir/src/dllmain.cpp.obj"
+  "libBrightNights.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BrightNights.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
