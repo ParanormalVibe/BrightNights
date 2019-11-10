7ba4ee2ef7b15ca4f0c9816df6e1b5c951715af7
ParanormalVibe <foo@fennel.io>
2019-11-10 04:04:17 -0500

    oof

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 #include "cwmods/cwmods.h"
 
-
 class BrightNightWrapper : GenericMod {
 	virtual void Initialize() override {
 		LoadLibraryA("Mods/BrightNight.cwmod");
 	}
 };
 
-EXPORT BrightNightWrapper* ModakeMod() {
+EXPORT BrightNightWrapper* MakeMod() {
 	return new BrightNightWrapper();
 }
